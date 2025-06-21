58b777f3d30b7d83412c610e187c12e7507d0576
Sourav-sudow <souravkumar8oct@gmail.com>
2025-06-21 23:44:58 +0530

    Initial commit

---
+++ src/sections/About.jsx
@@ -0,0 +1,68 @@
+import { useRef } from "react";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { AnimatedTextLines } from "../components/AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const About = () => {
+  const text = `Tech enthusiast building secure, data-driven solutions
+    From cybersecurity to cloud ops, I bring ideas to life
+    Blending creativity, performance & precision in every project`;
+
+  const aboutText = `BCA student and passionate tech explorer with hands-on experience in IT Operations, Cybersecurity, and Digital Marketing. Currently an intern at TKV Solutions, I’ve worked on everything from analyzing password security to promoting digital platforms.
+
+Skilled in Big Data, Data Analytics, Google Workspace, and Cloud Security, I love solving real-world problems through code and research.
+
+When I’m not interning or upskilling:
+🔐 Exploring ethical hacking and cybersecurity tools  
+📊 Diving into data with IBM & Deloitte certifications  
+🚀 Collaborating on hackathons and open-source experiments  
+🎓 Always learning—because curiosity fuels growth`;
+
+  const imgRef = useRef(null);
+  useGSAP(() => {
+    gsap.to("#about", {
+      scale: 0.95,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "bottom 80%",
+        end: "bottom 20%",
+        scrub: true,
+        markers: false,
+      },
+      ease: "power1.inOut",
+    });
+
+    gsap.set(imgRef.current, {
+      clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
+    });
+    gsap.to(imgRef.current, {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      duration: 2,
+      ease: "power4.out",
+      scrollTrigger: { trigger: imgRef.current },
+    });
+  });
+  return (
+    <section id="about" className="min-h-screen bg-black rounded-b-4xl">
+      <AnimatedHeaderSection
+        subTitle={"Cod with purpose, Built to scale"}
+        title={"About"}
+        text={text}
+        textColor={"text-white"}
+        withScrollTrigger={true}
+      />
+      <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
+        <img
+          ref={imgRef}
+          src="images/man.jpg"
+          alt="man"
+          className="w-md rounded-3xl"
+        />
+        <AnimatedTextLines text={aboutText} className={"w-full"} />
+      </div>
+    </section>
+  );
+};
+
+export default About;
